ae7675987be2876a02f1bd4212e9e074aa840b98
Ritik Puranik <ritikpuranik28@gmail.com>
2026-08-31 04:59:34 +0530

    Time: 12 ms (94.51%), Space: 49.2 MB (51.74%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] distance = new int[m][n];
+        Queue<int[]> queue = new LinkedList<>();
+        int[][] directions = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) queue.offer(new int[] {i, j});
+                else distance[i][j] = -1;
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int row = current[0];
+            int col = current[1];
+
+            for (int[] dir : directions) {
+                int newRow = row + dir[0];
+                int newCol = col + dir[1];
+
+                if  (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && distance[newRow][newCol] == -1) {
+                    distance[newRow][newCol] = distance[row][col] + 1;
+                    queue.offer(new int[]{newRow, newCol});
+                }
+            }
+        }
+        return distance;
+    }
+}
